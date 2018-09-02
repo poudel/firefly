8b3196720372c90f0038cff2d1a06dea7486b66f
keshaB Paudel <self@keshab.net>
2018-09-03 01:00:08 +0545

    added config for target link clickability

--- firefly/links.py
+++ firefly/links.py
@@ -58,6 +58,7 @@ def links():
     ]
     config = {
         "show_target_link": True,
+        "make_target_link_clickable": True,
         "show_target_domain": True,
         "open_new_tab": True,
         "show_copy_link": True,
