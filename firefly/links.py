1ea1361c536307561f6d5e641f91c1d702d7d9fd
keshaB Paudel <self@keshab.net>
2018-09-02 12:58:59 +0545

    added navbar, default link options etc

--- firefly/links.py
+++ firefly/links.py
@@ -4,12 +4,19 @@ from flask import Blueprint, render_template
 bp = Blueprint("links", __name__, url_prefix="/links")
 
 
+def update_defaults(l):
+    link = {"new_tab": True}
+    link.update(l)
+    return link
+
+
 @bp.route("/")
 def links():
+
     links = [
-        {"url": "https://google.com", "title": "Google"},
+        {"url": "https://google.com", "title": "Google", "new_tab": False},
         {"url": "https://facebook.com", "title": "Facebook"},
         {"url": "https://twitter.com", "title": "Twitter"},
         {"url": "https://instagram.com", "title": "Instagram"},
     ]
-    return render_template("links.html", links=links)
+    return render_template("links.html", links=map(update_defaults, links))
