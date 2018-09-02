8440b95a02895e38c0e88eb63ab133595db026ca
keshaB Paudel <self@keshab.net>
2018-09-02 11:18:04 +0545

    added template html

--- firefly/links.py
+++ firefly/links.py
@@ -6,4 +6,10 @@ bp = Blueprint("links", __name__, url_prefix="/links")
 
 @bp.route("/")
 def links():
-    return render_template("links.html")
+    links = [
+        {"url": "https://google.com", "title": "Google"},
+        {"url": "https://facebook.com", "title": "Facebook"},
+        {"url": "https://twitter.com", "title": "Twitter"},
+        {"url": "https://instagram.com", "title": "Instagram"},
+    ]
+    return render_template("links.html", links=links)
